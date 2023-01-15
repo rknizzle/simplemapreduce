fef262b5ccd95d7a670e7d2182f64937968ee38a
rknizzle <ryan@autotiv.com>
2023-01-15 12:02:59 -0500

    Store the endpoint of the worker in the client rather...
    ... than in the workerState struct
    
    Also add a function workerState.sendTaskToWorker thats just a wrapper
    around calling the clients callStartTask function

--- pkg/coordinator/workerState.go
+++ pkg/coordinator/workerState.go
@@ -1,15 +1,23 @@
 package coordinator
 
 type workerClient interface {
-	callStartTask()
+	callStartTask(task *Task) error
 }
 
 // workerState stores the state of a single worker node from the perspective of the Coordinator
 type WorkerState struct {
 	isHealthy   bool
 	isBusy      bool
-	url         string
 	currentTask *Task
 
 	client workerClient
+}
+
+func (w WorkerState) sendTaskToWorker(task *Task) error {
+	err := w.client.callStartTask(task)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- pkg/coordinator/worker_rpc_client.go
+++ pkg/coordinator/worker_rpc_client.go
@@ -1,9 +1,52 @@
 package coordinator
 
-// WorkerRPCclient is a client for the Coordinator to communicate with the worker nodes via RPC
-type workerRPCclient struct{}
+import (
+	"net/rpc"
 
-func (w workerRPCclient) callStartTask() {
-	// send RPC to the worker to start working on a specific task
-	// call(TODO)
+	"github.com/rknizzle/simplemapreduce/pkg/worker"
+)
+
+// WorkerRPCclient is a client for the Coordinator to communicate with the worker nodes via RPC
+type workerRPCclient struct {
+	// endpoint is usually either a unix socket when running locally or a URL when calling across the network
+	endpoint     string
+	isUnixSocket bool
+	isTCP        bool
+}
+
+func (w workerRPCclient) callStartTask(task *Task) error {
+	args := &worker.StartTaskArgs{
+		JobID:        task.JobID,
+		TaskID:       task.ID,
+		Plugin:       task.Plugin,
+		Inputs:       task.Inputs,
+		IsMapTask:    task.IsMapTask,
+		IsReduceTask: task.IsReduceTask,
+	}
+	reply := &worker.StartTaskReply{}
+
+	err := call("worker.RPCserver.HandleStartTask", w.endpoint, args, reply)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// send RPC
+func call(rpcname string, endpoint string, args interface{}, reply interface{}) error {
+	// TODO: have the 'unix' be configurable to 'tcp' when running over the network
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	c, err := rpc.DialHTTP("unix", endpoint)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
