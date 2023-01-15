841f11d638eb4fe9de5a261061a21f83db37839e
rknizzle <ryan@autotiv.com>
2023-01-15 12:08:07 -0500

    Pass the proper args to the startTask method from the RPC handler

--- pkg/worker/rpc_server.go
+++ pkg/worker/rpc_server.go
@@ -1,8 +1,10 @@
 package worker
 
+import "fmt"
+
 // worker defines the logic that this API needs to use
 type worker interface {
-	startTask()
+	startTask(string, string, string, []string, bool, bool)
 }
 
 // RPCserver is a server for the Worker that listens for RPC calls from the coordinator node
@@ -10,11 +12,27 @@ type RPCserver struct {
 	w worker
 }
 
-type StartTaskArgs struct{}
+type StartTaskArgs struct {
+	JobID        string
+	TaskID       string
+	Plugin       string
+	Inputs       []string
+	IsMapTask    bool
+	IsReduceTask bool
+}
 type StartTaskReply struct{}
 
 func (server RPCserver) HandleStartTask(args *StartTaskArgs, reply *StartTaskReply) {
-	server.w.startTask()
+	fmt.Println("DEBUG: worker server recieved an RPC call to start a task")
+
+	server.w.startTask(
+		args.JobID,
+		args.TaskID,
+		args.Plugin,
+		args.Inputs,
+		args.IsMapTask,
+		args.IsReduceTask,
+	)
 }
 
 func (server RPCserver) startServer() error {
